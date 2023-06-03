c551110ae536442a22ae25fa95aca7c7b712a9be
Elias Souza <osouzaelias@gmail.com>
2023-06-03 11:29:56 -0300

    ajustes

--- handlers.go
+++ handlers.go
@@ -21,7 +21,7 @@ var users = map[int]User{
 }
 
 // Rota para buscar um usuário por ID
-func getUserByIDHandler(w http.ResponseWriter, r *http.Request) {
+func GetUserByIDHandler(w http.ResponseWriter, r *http.Request) {
 	// Obtém o ID do usuário a partir dos parâmetros da rota
 	idStr := r.URL.Path[len("/api/user/"):]
 	id, err := strconv.Atoi(idStr)
@@ -55,7 +55,7 @@ func getUserByIDHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Rota que retorna os dados de um usuário específico
-func getUserHandler(w http.ResponseWriter, r *http.Request) {
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	// Simulando um usuário retornado de um banco de dados ou outra fonte de dados
 	user := User{
 		ID:       1,
@@ -75,5 +75,35 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// Escrevendo a resposta
+	w.Write(response)
+}
+
+// Estrutura para representar o status de saúde do serviço
+type HealthStatus struct {
+	Status string `json:"status"`
+}
+
+// Rota para verificar o status de saúde do serviço
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Verifica se o serviço está em execução corretamente
+	// Você pode adicionar lógica adicional aqui, se necessário
+
+	// Cria uma estrutura de status de saúde
+	status := HealthStatus{
+		Status: "UP",
+	}
+
+	// Serializa a estrutura de status para JSON
+	response, err := json.Marshal(status)
+	if err != nil {
+		log.Println("Erro ao serializar status de saúde para JSON:", err)
+		http.Error(w, "Erro interno do servidor", http.StatusInternalServerError)
+		return
+	}
+
+	// Define o cabeçalho de resposta como JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Escreve a resposta
 	w.Write(response)
 }
--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	server := http.NewServeMux()
 
-	server.HandleFunc("/api/user", getUserHandler)
-	server.HandleFunc("/health", healthCheckHandler)
+	server.HandleFunc("/api/user", GetUserHandler)
+	server.HandleFunc("/health", HealthCheckHandler)
 
 	// Rota para buscar usuário por ID
-	server.HandleFunc("/api/user/", getUserByIDHandler)
+	server.HandleFunc("/api/user/", GetUserByIDHandler)
 
 	log.Println("Servidor iniciado na porta 8080")
 	err := http.ListenAndServe(":8080", server)
